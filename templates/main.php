8d617de58fbce6bab040792ec660964781453ea4
dez <dezmandd1@gmail.com>
2021-01-27 21:37:27 +0500

    refactor: поменял url для просмотра конкретного лота

--- templates/main.php
+++ templates/main.php
@@ -29,7 +29,7 @@
                         </div>
                         <div class="lot__info">
                             <span class="lot__category"><?=htmlspecialchars($product['title_cat'])?></span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($product['title'])?></a></h3>
+                            <h3 class="lot__title"><a class="text-link" href="/lot.php?id=<?=$product['lot_id']?>"><?=htmlspecialchars($product['title'])?></a></h3>
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Стартовая цена</span>
