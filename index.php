0d18e50f89e7e4125c4b9dccc34a4e759d9cb191
dez <dezmandd1@gmail.com>
2021-01-27 21:39:26 +0500

    refactor: перенес sql запросы в функции

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $is_auth = rand(0, 1);
 
 $user_name = 'dez'; // укажите здесь ваше имя
 
-$categories = query("SELECT title, slug FROM categories");
-$products = query("SELECT l.id as lot_id, l.title, l.price, l.img, l.date_out, r.price as new_price, c.title as title_cat FROM lot l LEFT JOIN rate r ON l.id = r.lot_id JOIN categories c ON l.cat_id = c.id WHERE l.date_out >= NOW() ORDER BY l.date_up DESC");
+$categories = get_categories();
+$products = get_active_lot();
 
 $main = include_template('main.php', $data = ['categories' => $categories, 'products' => $products]);
 $layout = include_template('layout.php', $data = ['content' => $main, 'title' => 'Главная',
